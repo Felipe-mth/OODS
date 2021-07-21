cae0619152dc624ae24bdcb1cf91ac7beb3c7fef
Felipe Matheus Costa <86928483+Felipe-mth@users.noreply.github.com>
2021-07-20 23:12:30 -0300

    Add files via upload

---
+++ Aula 1/src/Ola.java
@@ -0,0 +1,11 @@
+/*1.  Altere	seu	programa	para	imprimir	uma	mensagem	diferente.
+2.	 Altere	seu	programa	para	imprimir	duas	linhas	de	texto	usando	duas	linhas	de	código	System.out.
+3.	 Sabendo	 que	 os	 caracteres	 	\n		 representam	 uma	 quebra	 de	linhas,	imprima	 duas	linhas	 de	texto
+usando	uma	única	linha	de	código		System.out	.*/
+
+public class Ola {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Ola!! \ntudo bem?");
+        System.out.println("Tenha um bom dia");
+    }
+}
