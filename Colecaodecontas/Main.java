cf4a0133ec2feb78cbf404283ba14f73e59ce693
Felipe Matheus Costa <86928483+Felipe-mth@users.noreply.github.com>
2021-08-14 00:15:22 -0300

    Atualizando main

--- Colecaodecontas/Main.java
+++ Colecaodecontas/Main.java
@@ -9,21 +9,41 @@ public class Main {
         Colecao co = new Colecao();
         Conta c1= new Conta(1);
         Conta c2= new Conta(2);
+        Poupança p = new Poupança(3);
         
-        c1.setNome("Jorge");
-        c1.setSaldo(10000);
+        System.out.println(c1 instanceof Poupança);
+        System.out.println(p instanceof Poupança);
         
-        c2.setNome("Lucas");
-        c2.setSaldo(5000);
-        
+       
+       c1.Creditar(500);
+       c2.Creditar(1500);
+       p.Creditar(100);
+       p.renderJuros((float) 0.1);
+       
+       System.out.println(c1.getSaldo());
+       System.out.println(c2.getSaldo());
+       
+       c1.Debitar(1000);
+       
+       System.out.println(c1.getSaldo());
+       
+       c2.Debitar((float) 250);
+       
+       c2.Debitar((float) 150);
+       
+       System.out.println(c2.getSaldo());
         co.Adicionar(c1);
         co.Adicionar(c2);
+        co.Adicionar(p);
         
         co.Listar();
         
         co.Remover(2);
        
         co.Listar();
+        
+        co.renderPoupanca();
+        
         
         
         
